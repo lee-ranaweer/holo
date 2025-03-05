33615a508517cc8ae69134624dbce549670ff7eb
lee-ranaweer <nranawee@uoguelph.ca>
2025-03-05 18:44:06 -0500

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'router.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,122 +7,10 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext content) {
+  Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: _router,
-      title: 'Navbar'
+      routerConfig: router,
+      title: 'Navbar',
     );
-  }
-}
-
-final GoRouter _router = GoRouter(
-  initialLocation: '/',
-  routes: [
-    ShellRoute(
-      builder: (context, state, child) {
-        return NavBar(child: child);
-      },
-      routes: [
-        GoRoute(path: '/', builder: (context, state) => HomePage()),
-        GoRoute(path: '/collections', builder: (context, state) => CollectionsPage()), 
-        GoRoute(path: '/search', builder: (context, state) => SearchPage()),
-        GoRoute(path: '/notifications', builder: (context, state) => NotificationsPage()),
-        GoRoute(path: '/account', builder: (context, state) => AccountPage()),
-      ],
-    ),
-  ],
-);
-
-class NavBar extends StatelessWidget {
-  final Widget child;
-
-  NavBar({required this.child});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: child,
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _getSelectedIndex(context),
-        backgroundColor: Colors.blue,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.white70,
-        
-        onTap: (index) {
-          switch (index) {
-            case 0:
-              context.go('/');
-              break;
-            case 1:
-              context.go('/collections');
-              break;
-            case 2:
-              context.go('/search');
-              break;
-            case 3:
-              context.go('/notifications');
-              break;
-            case 4:
-              context.go('/account');
-              break;
-          }
-        },
-
-        // NOTE: Icons found here https://api.flutter.dev/flutter/material/Icons-class.html
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.collections), label: 'Collections'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Icons.notifications), label: 'Notifications'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Account'),
-        ],
-      ),
-    );
-  }
-
-  int _getSelectedIndex(BuildContext context) {
-    final location = GoRouterState.of(context).uri.toString();
-    if (location == '/')              return 0;
-    if (location == '/collections')   return 1;
-    if (location == '/search')        return 2;
-    if (location == '/notifications') return 3;
-    if (location == '/account')       return 4;
-    return 0;
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Home Page', style: TextStyle(fontSize: 24)));
-  }
-}
-
-class CollectionsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Collections Page', style: TextStyle(fontSize: 24)));
-  }
-}
-
-class SearchPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Search Page', style: TextStyle(fontSize: 24)));
-  }
-}
-
-class NotificationsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Notifications Page', style: TextStyle(fontSize: 24)));
-  }
-}
-
-class AccountPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Account Page', style: TextStyle(fontSize: 24)));
   }
 }
---
+++ lib/pages/collections_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CollectionsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Collections Page', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
